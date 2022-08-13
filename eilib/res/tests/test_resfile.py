8a94329ce2e43b8ab396e4f1aebadd33caa74adc
Yury Kotov <koteyur@gmail.com>
2022-08-13 12:30:28 +0200

    Fix writing inner resfiles

--- eilib/res/tests/test_resfile.py
+++ eilib/res/tests/test_resfile.py
@@ -21,10 +21,10 @@ class TestResFile(unittest.TestCase):
         with ResFile(resfile, 'w'):
             pass
         self.assertEqual(resfile.getvalue(), b'<\xe2\x9c\x01' + b'\0' * 12)
+        self.check_resfile_content(resfile, {})
 
         # Test resfile consistency
         test_files = {'foo': b'1234567', 'bar': b'hello world', 'rab': b'aaaabbbbaaaabbbb'}
-        resfile.seek(0)
         with ResFile(resfile, 'w') as res:
             for name, data in test_files.items():
                 with res.open(name, 'w') as file:
@@ -36,12 +36,32 @@ class TestResFile(unittest.TestCase):
             'foo': b'fooooo-old',  # Update old file
             'foo-new': b'foooooo-new',  # New file
         }
-        resfile.seek(0)
         with ResFile(resfile, 'a') as res:
             for name, data in test_files_upd.items():
                 with res.open(name, 'w') as file:
                     file.write(data)
         self.check_resfile_content(resfile, {**test_files, **test_files_upd})
+
+    def test_inner_resfile(self):
+        resfile_out = io.BytesIO()
+
+        # Create archive.
+        with (
+            ResFile(resfile_out, "w") as res_out,
+            res_out.open("inner.res", "w") as resfile_in,
+            ResFile(resfile_in, "w") as res_in,
+            res_in.open("file.txt", "w") as file,
+        ):
+            file.write(b"hello world")
+
+        # Check content.
+        with (
+            ResFile(resfile_out, "r") as res_out,
+            res_out.open("inner.res", "r") as resfile_in,
+            ResFile(resfile_in, "r") as res_in,
+            res_in.open("file.txt", "r") as file,
+        ):
+            self.assertEqual(file.read(), b"hello world")
 
     def check_resfile_content(self, resfile, test_files):
         resfile.seek(0)
@@ -49,6 +69,9 @@ class TestResFile(unittest.TestCase):
             for name, data in test_files.items():
                 with res.open(name) as file:
                     self.assertEqual(file.read(), data)
+            for info in res.iter_files():
+                self.assertIn(info.name, test_files)
+        resfile.seek(0)
 
 
 if __name__ == '__main__':
